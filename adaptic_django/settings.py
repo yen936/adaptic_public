4ee470fd97b2f7d537de250bd85748a69d05eaab
Yen <43019003+yen936@users.noreply.github.com>
2024-05-27 11:54:19 -0400

    Update settings.py

--- adaptic_django/settings.py
+++ adaptic_django/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-($+6qqsk_^y*b6ck22&x-w1wr2ci9p*ai%m)l4xcd=yzz#)xp'
+SECRET_KEY = "<update-with-your-secret>"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
